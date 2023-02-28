0cf207d59d6741d10e4933bc043862b21a606304
MbIPO4KA <63498319+MbIPO4KA@users.noreply.github.com>
2023-02-28 09:43:14 +0500

    Задача решена правильно, с проверкой на отр. знаки

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Write("Введите число А: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+int sum = 0;
+if (number<0)  number =  number * (-1);
+while (number > 0)
+{
+    sum = sum + number % 10;
+    number = number /10 ; 
+}
+    Console.WriteLine($"Cумма чисел в числе равна {sum} ");
